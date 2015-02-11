846227ceb65d25d732764f6d1dd7edff5e516f00
teamholmes <team.holmes@gmail.com>
2015-02-11 17:34:09 +0000

    adding angular framework support

---
+++ WebApplication/WebApplication/Scripts/angular/app.js
@@ -0,0 +1,126 @@
+﻿var appModule = angular.module('reviewmywebsite', []);
+
+appModule.controller('loginController', ['$scope', '$http', function ($scope, $http) {
+
+    var connectionWelcomeText = "Angular connected";
+
+
+    $scope.init = function () {
+
+        $scope.loadingText = connectionWelcomeText;
+
+        alert("loaded");
+
+    };
+
+
+    $scope.nameer = "World";
+
+
+
+    $scope.init();
+
+}]);
+
+
+
+
+
+
+
+//var appModule = angular.module('appName', []);
+
+
+//appModule.controller('certificateControllerController', ['$scope', '$http', function ($scope, $http) {
+
+//    var loadingTextDefault = "Loading data... Please wait.";
+//    var errorrTextDefault = "Error loading data.";
+
+
+
+//    function setStateOfIncludeAdditionalTeamMembers() {
+//        if ($scope.certificateForEachTeamMember == false) {
+//            $scope.includeAdditionalSupportTeamMembers = false;
+//        }
+//    }
+
+//    //var url = @Url.Action("GetEventTeamsByPhaseApproved", "DevTool")
+//    //alert(url);
+
+
+//    $scope.loadJson = function () {
+
+//        $scope.isLoading = true;
+//        $scope.eventteams = {};
+//        $scope.loadingtext = loadingTextDefault;
+//        $http.get('http://localhost:12186/DevTools/GetEventTeamsByPhaseApproved', {
+//            params: { phaseId: $scope.phaseobject }
+
+//        }).success(function (data, status, headers, config) {
+//            $scope.loadingtext = "";
+//            $scope.eventteams = data.result;
+//            $scope.isLoading = false;
+//        }).error(function (data, status, headers, config) {
+//            $scope.loadingtext = errorrTextDefault + " " + status + " " + data;
+//            $scope.isLoading = false;
+//        });
+//    };//    $scope.checkboxClick = function (itm) {
+//        //alert(itm.Selected);
+//        //alert("hello");
+//    };//    $scope.selectAll = function () {
+//        //alert(itm.Selected);
+//        for (var i = 0, l = $scope.eventteams.length; i < l; i++) {
+//            $scope.eventteams[i].Selected = true;
+//        }
+//        //alert("hello");
+//    };//    $scope.deSelectAll = function () {
+//        //alert(itm.Selected);
+//        for (var i = 0, l = $scope.eventteams.length; i < l; i++) {
+//            $scope.eventteams[i].Selected = false;
+//        }
+//       // alert("bhello");
+//    };//    $scope.teamOnlyChange = function () {
+//        //alert(itm.Selected);
+//        setStateOfIncludeAdditionalTeamMembers()
+//        // alert("bhello");
+//    };//    //$scope.sendSchool = function (input) {
+
+//    //    var postData = { schoolname: '"' + input + '"'}
+//    //    $http.post('http://localhost:15940/Home/ShowInsitutionspost',postData, {
+//    //        params: { schoolname: input }
+//    //    }).success(function (data, status, headers, config) {
+//    //        $scope.schools = data.result;
+//    //    }).error(function (data, status, headers, config) {
+//    //        alert("there has been an error " + status + " " + data);
+//    //    });
+//    //};
+//    //$scope.parseInt = function (value) {
+//    //        return parseInt(value);
+//    //    }
+
+
+
+//    //$scope.reset = function () {
+//    //    $scope.init();
+//    //};
+
+
+
+
+
+//    $scope.init = function () {
+//        $scope.phaseobject = 2;
+//        $scope.isLoading = false;
+//        $scope.certificateForEachTeamMember = false;
+//        $scope.includeAdditionalSupportTeamMembers = false;
+//        $scope.loadJson();
+//        //$scope.lastName = "Doeey";
+//        //$scope.newschoolname = "";
+//        //$scope.sex = "Oh no - we are british so dont take the bus shelters with us";
+//    };
+
+
+//    $scope.init();
+
+//}]);
+
